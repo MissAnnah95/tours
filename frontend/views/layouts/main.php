5515a92cb04f2d65d1782c0a1256916c32230fad
MissAnnah95 <mbicomaryann@gmail.com>
2020-11-13 14:19:35 +0300

    Progress

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -21,6 +21,8 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
     <?php $this->head() ?>
 </head>
 <body>
@@ -36,7 +38,7 @@ AppAsset::register($this);
         ],
     ]);
 
-    
+
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
     ]);
