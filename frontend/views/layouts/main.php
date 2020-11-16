17379007804ac1e4b2f1ef20e5c4d77ad308d240
Sil silas <silaswans@gmail.com>
2020-11-16 14:25:13 +0300

    latest changes

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,10 +26,45 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-<div class="top-nav">
- <p class="covid">Covid-20 news ...</p>
-</div>
+    <!-- <div class="top-nav">
+    <p class="covid">Covid-20 news ...</p>
+    </div> -->
+<!--Navbar -->
+<nav class="mb-0 navbar navbar-expand-lg">
+<div class="view overlay z-depth-1-half">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQfz1dfY2oFcNNUBr4U47XAxeNaQgZlecucbg&usqp=CAU" class="" height="50px" width="50px" alt="avatar">
+        </div>
+                <a class="navbar-brand logo font-bold" href="#">African<span class="">Tours</span></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
+                    <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-map-marker"></i> Destinations</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-plane"></i> Tours</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-usps"></i> Packages</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#"><i class="fa fa-envelope"></i> Contact <span class="sr-only">(current)</span></a>
+                        </li>
 
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i> Profile </a>
+                            <div class="dropdown-menu dropdown-menu-right dropdown-cyan" aria-labelledby="navbarDropdownMenuLink-4">
+                                <a class="dropdown-item" href="#">My account</a>
+                                <a class="dropdown-item" href="#">Log out</a>
+                                <a class="dropdown-item" href="#">Change Profile</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!--/.Navbar -->
 <div class="wrap">
     <!-- <?php
     NavBar::begin([
