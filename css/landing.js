2b9c8aacfcf0c4e9084a92c310fec06f55a2cf15
Sil silas <silaswans@gmail.com>
2020-11-16 11:13:29 +0300

    landing changes

--- css/landing.js
+++ css/landing.js
@@ -140,3 +140,28 @@
               template: '<div class="tooltip md-tooltip"><div class="tooltip-arrow md-arrow"></div><div class="tooltip-inner md-inner"></div></div>'
             });
           })
+
+
+
+          $(window).scroll(function(){
+            $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+          });
+
+
+          // Startup Scripts
+$(document).ready(function()
+{
+	$('.hero').css('height', ($(window).height() - $('header').outerHeight()) + 'px'); // Set hero to fill page height
+
+	$('#scroll-hero').click(function()
+	{
+		$('html,body').animate({scrollTop: $("#hero-bloc").height()}, 'slow');
+	});
+});
+
+
+// Window resize
+$(window).resize(function()
+{
+	$('.hero').css('height', ($(window).height() - $('header').outerHeight()) + 'px'); // Refresh hero height
+}); 
