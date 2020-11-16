c7bed2c2d5b3aaa287b8c85ee58f72483b6fab16
Sil silas <silaswans@gmail.com>
2020-11-17 02:55:56 +0300

    final landing page

---
+++ frontend/views/site/_services.php
@@ -0,0 +1,36 @@
+<div class="container p-3 pt-lg-5">
+<div class="search row mb-4 m-3">
+  <div class="col-sm-4">
+    <div class="card border-3">
+      <div class="card-body">
+        <div class="d-flex p-md-2">
+        <i class="fa fa-send services-icon  p-1 my-1 mr-3"></i>
+        <h5 class="services">Best Selection</h5>
+        </div>
+        <p class=".services-p">Our strict screening process means that you're only seeing the best quality tours.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card border-0">
+      <div class="card-body">
+        <div class="d-flex p-md-2">
+        <i class="fa fa-tag services-icon  p-1 my-1 mr-3"></i>
+        <h5 class="services">Best Price Guarantee</h5>
+        </div>
+        <p class=".services-p">Found a lower price elsewhere? We'll match it!</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card border-0">
+      <div class="card-body">
+        <div class="d-flex p-md-2">
+        <i class="fa fa-volume-control-phone services-icon  p-1 my-1 mr-3"></i>
+        <h5 class="services">24/7 Support</h5>
+        </div>
+        <p class=".services-p">We're available to answer any questions or concerns via online chat, phone or email.</p>
+      </div>
+    </div>
+  </div>
+</div>
