8851b8c82588f8017b5f2d2d85d0a1a09e786872
warui-david <waruidavid57@gmail.com>
2020-11-17 10:56:15 +0300

    new update

--- frontend/views/site/listings.php
+++ frontend/views/site/listings.php
@@ -1,33 +1,179 @@
-
-<div class="col-12">
+<?php
+use yii\helpers\Url;
+?>
+<div class="container">
   <h1> Deals and Discounts in African Tours</h1>
   <hr style="color:gray;background-color:gray">
 </div>
-
 <!-- Listings Filter -->
 <div class="container">
 <div class="row">
-
  <div class="col-3">
-
      <button type="button" class="btn btn-primary btn-lg"><i class="fa fa-filter"></i>Sort & Filter </button>
      <br></br>
-
-     <div class="card shadow-sm" style="width: 16rem;">
-       <ul class="list-group list-group-flush">
-         <li class="list-group-item">Cras justo odio</li>
-         <li class="list-group-item">Dapibus ac facilisis in</li>
-         <li class="list-group-item">Vestibulum at eros</li>
-         <li class="list-group-item">Vestibulum at eros</li>
-         <li class="list-group-item">Vestibulum at eros</li>
-         <li class="list-group-item">Vestibulum at eros</li>
-       </ul>
+     <div class="accordion shadow-sm" id="accordionExample">
+   <div class="card">
+     <div class="card-header" id="headingOne">
+       <h2 class="mb-0">
+         <button class="btn btn-light btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+           Departure Period
+         </button>
+       </h2>
+       </div>
+     <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+       <div class="card-body">
+         <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck1">
+          <label class="form-check-label" for="gridCheck1">
+            December 2020
+          <br></br>
+          </label>
+          </div>
+          <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck2">
+          <label class="form-check-label" for="gridCheck2">
+            January 2021
+          <br></br>
+          </label>
+        </div>
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck3">
+        <label class="form-check-label" for="gridCheck3">
+          February 2021
+        <br></br>
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck4">
+        <label class="form-check-label" for="gridCheck4">
+          March 2021
+        <br></br>
+        </label>
+      </div>
+       </div>
      </div>
+   </div>
+   <div class="card">
+     <div class="card-header" id="headingTwo">
+       <h2 class="mb-0">
+         <button class="btn btn-light btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+           Group Type
+         </button>
+       </h2>
+     </div>
+     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+       <div class="card-body">
+         <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck1">
+          <label class="form-check-label" for="gridCheck1">
+            Independent
+            <br></br>
+          </label>
+          </div>
+          <div class="form-check">
+           <input class="form-check-input" type="checkbox" id="gridCheck2">
+           <label class="form-check-label" for="gridCheck2">
+             Group
+             <br></br>
+           </label>
+           </div>
+           <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck3">
+            <label class="form-check-label" for="gridCheck3">
+              Private
+              <br></br>
+            </label>
+            </div>
+       </div>
+     </div>
+   </div>
+   <div class="card">
+     <div class="card-header" id="headingThree">
+       <h2 class="mb-0">
+         <button class="btn btn-light btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+           Age Range
+         </button>
+       </h2>
+     </div>
+     <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+       <div class="card-body">
+         <button type="button" class="btn btn-light">18 - 39</button>
+         <button type="button" class="btn btn-light">40+</button>
+         <br></br>
+         <button type="button" class="btn btn-light">60+</button>
+         <button type="button" class="btn btn-light">80+</button>
+       </div>
+     </div>
+   </div>
+   <div class="card">
+     <div class="card-header" id="headingThree">
+       <h2 class="mb-0">
+         <button class="btn btn-light btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+           Listing Type
+         </button>
+       </h2>
+     </div>
+     <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+       <div class="card-body">
+         <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck1">
+          <label class="form-check-label" for="gridCheck1">
+            Camping
+            <br></br>
+          </label>
+          </div>
+          <div class="form-check">
+           <input class="form-check-input" type="checkbox" id="gridCheck2">
+           <label class="form-check-label" for="gridCheck2">
+             Hiking
+             <br></br>
+           </label>
+           </div>
+           <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck3">
+            <label class="form-check-label" for="gridCheck3">
+              Cultural Tourism
+              <br></br>
+            </label>
+            </div>
+            <div class="form-check">
+             <input class="form-check-input" type="checkbox" id="gridCheck4">
+             <label class="form-check-label" for="gridCheck4">
+              Sports and Recreation Tourism
+               <br></br>
+             </label>
+             </div>
+             <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck5">
+              <label class="form-check-label" for="gridCheck5">
+                Nature and Adventure Tourism
+                <br></br>
+              </label>
+              </div>
+       </div>
+     </div>
+   </div>
+   <div class="card">
+     <div class="card-header" id="headingThree">
+       <h2 class="mb-0">
+         <button class="btn btn-light btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+           Price Range
+         </button>
+       </h2>
+     </div>
+     <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+       <div class="card-body">
+         <form>
+          <div class="form-group">
+            <label for="formControlRange">Example Range input</label>
+            <input type="range" class="form-control-range" id="formControlRange">
+          </div>
+        </form>
+       </div>
+     </div>
+   </div>
+ </div>
   </div>
-
-
-
-
 <div class="col-9">
      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
            <div class="card" style="width: 18rem;">
@@ -36,27 +182,21 @@
              </div>
            </div>
             <div class="col p-4 d-flex flex-column position-static">
-
               <div class="mb-1 text-muted">Hiking</div>
-
               <h3 class="mb-0">Gede Ruins
-
                 <i class="fa fa-star-o" aria-hidden="true"></i>
                 <i class="fa fa-star-o" aria-hidden="true"></i>
                 <i class="fa fa-star-o" aria-hidden="true"></i>
                 <i class="fa fa-star-o" aria-hidden="true"></i>
                 <i class="fa fa-star-o" aria-hidden="true"></i>
                 <hr style="color:gray;background-color:gray"></h3>
-
               <p class="card-text mb-auto">"This is a wider card with supporting text below as a natural lead-in to additional content."</p>
               <strong class="d-inline-block mb-2 text-body">Tour Operator</strong>
               <strong class="d-inline-block mb-2 text-body">Age Range</strong>
               <strong class="d-inline-block mb-2 text-body">Price</strong>
-              <a href="#" class="stretched-link">View Tour</a>
+              <a href="<?= Url::to(['/specifictour/tour'])?>" class="stretched-link">View Tour</a>
             </div>
-
           </div>
-
           <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="card" style="width: 18rem;">
                   <img src="<?= Yii::$app->request->baseUrl ?>/listingsimages/t2.jpeg" class="card-img-top" alt="...">
@@ -64,27 +204,21 @@
                   </div>
                 </div>
                  <div class="col p-4 d-flex flex-column position-static">
-
                    <div class="mb-1 text-muted">Hiking</div>
-
                    <h3 class="mb-0">Gede Ruins
-
                      <i class="fa fa-star-o" aria-hidden="true"></i>
                      <i class="fa fa-star-o" aria-hidden="true"></i>
                      <i class="fa fa-star-o" aria-hidden="true"></i>
                      <i class="fa fa-star-o" aria-hidden="true"></i>
                      <i class="fa fa-star-o" aria-hidden="true"></i>
                      <hr style="color:gray;background-color:gray"></h3>
-
                    <p class="card-text mb-auto">"This is a wider card with supporting text below as a natural lead-in to additional content."</p>
                    <strong class="d-inline-block mb-2 text-body">Tour Operator</strong>
                    <strong class="d-inline-block mb-2 text-body">Age Range</strong>
                    <strong class="d-inline-block mb-2 text-body">Price</strong>
-                   <a href="#" class="stretched-link">View Tour</a>
+                   <a href="<?= Url::to(['/specifictour/tour'])?>" class="stretched-link">View Tour</a>
                  </div>
-
                </div>
-
                <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                      <div class="card" style="width: 18rem;">
                        <img src="<?= Yii::$app->request->baseUrl ?>/listingsimages/t4.jpg" class="card-img-top" alt="...">
@@ -92,28 +226,21 @@
                        </div>
                      </div>
                       <div class="col p-4 d-flex flex-column position-static">
-
                         <div class="mb-1 text-muted">Hiking</div>
-
                         <h3 class="mb-0">Gede Ruins
-
                           <i class="fa fa-star-o" aria-hidden="true"></i>
                           <i class="fa fa-star-o" aria-hidden="true"></i>
                           <i class="fa fa-star-o" aria-hidden="true"></i>
                           <i class="fa fa-star-o" aria-hidden="true"></i>
                           <i class="fa fa-star-o" aria-hidden="true"></i>
                           <hr style="color:gray;background-color:gray"></h3>
-
                         <p class="card-text mb-auto">"This is a wider card with supporting text below as a natural lead-in to additional content."</p>
                         <strong class="d-inline-block mb-2 text-body">Tour Operator</strong>
                         <strong class="d-inline-block mb-2 text-body">Age Range</strong>
                         <strong class="d-inline-block mb-2 text-body">Price</strong>
-                        <a href="#" class="stretched-link">View Tour</a>
+                        <a href="<?= Url::to(['/specifictour/tour'])?>" class="stretched-link">View Tour</a>
                       </div>
-
                     </div>
-
-
     </div>
   </div>
 </div>
