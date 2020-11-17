b2bee68e50a93afbbe41017e0d4315b5c5c896f8
warui-david <70076786+warui-david@users.noreply.github.com>
2020-11-17 10:12:13 +0300

    Revert "Booking"

--- frontend/views/site/booking.php
+++ frontend/views/site/booking.php
@@ -1,9 +1,19 @@
 <?php
 
 ?>
+<div class="wrap">
 
-<div class="body">
-
+    <h1> Kenya Wildlife Safari </h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="panel panel-default">
+                    <h2> 1. Review Your Dates </h2>
+                <div class="panel-body">Wednesday 11th November to Sunday 15th November<br><a href="#">Change Dates</a></br>
+                <div class="container mt-5 mb-5" style="width: 400px">
+                    <h3>Choose Date</h3>
+                    <input type="text" id="picker" class="form-control">
+                </div>
 
 <div class="wrap">
     <div class="container">
@@ -74,20 +84,49 @@
   <div class="row">
 
   </div>
-  
+  <br />
+    <div class="row">
+        <div class='col-sm-12'>
+            <div class="form-group">
+                <div class='input-group date' id='datetimepicker1'>
+                    <input type='text' class="form-control" />
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
 
-                   
+                    <!-- <input type="text" id="picker" class="form-control">
+                </div>
 
-                
+                <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajaz/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+                <script src="https://jquery.datetimepicker.full.min.js"></script>
+                <script>
+                    $('#picker').datetimepicker({
+                        timepicker:false,
+                        datepicker: true,
+                        format: 'Y-m-d',
+                        value: '2020-11-11'
+                    })
+                </script>
+            <div class="col-md-12 text-center">
+            <button type="button" class="btn btn-primary">Confirm dates</button>
+        </div>
+<<<<<<< HEAD
+                    </div>
+=======
 
             <div class="col-md-12 text-center">
             <button type="button" class="btn btn-primary">Confirm dates</button>
         </div>
                     </div>
 
+>>>>>>> 4de87fb985be4840f699eecee4bfddcbbc0132d7
     </div>
     </div>
     </div>
@@ -97,7 +136,31 @@
 
 <div class="container">
             <div class="row">
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
 
+                    <h2> 2. Traveler Details </h2>
+                        <div class="panel-body">
+
+                            <form>
+                                <div class="form-group">
+                                    <label>Full name</label>
+                                    <input class="form-control" placeholder="Full name">
+                                </div>
+                                <div class="form-group">
+                                    <label>Email address</label>
+                                    <input class="form-control" placeholder="Enter email">
+                                </div>
+                                <div class="form-group">
+                                    <label>Contact</label>
+                                    <input class="form-control" placeholder="Phone number">
+                                </div>
+                                <div class="form-group">
+                                    <label>Nationality</label>
+                                    <input class="form-control" placeholder="Country">
+                                </div>
+<<<<<<< HEAD
+=======
                     <h2> 2. Traveler Details </h2>
                     <div class="card bg-light mb-3">
                         <div class="card-body">
@@ -126,7 +189,7 @@
                                     <label>Nationality</label>
                                     <input class="form-control" placeholder="Country">
                                 </div>
-
+>>>>>>> 4de87fb985be4840f699eecee4bfddcbbc0132d7
                                 </div>
                                 <div class="col-md-12 text-center">
                                 <button type="submit" class="btn btn-primary">Add traveler</button>
@@ -141,7 +204,7 @@
 
         <div class="container">
             <div class="row">
-
+<<<<<<< HEAD
                 <div class="col-8">
                     
             <h2> 3. Payment </h2>
@@ -164,7 +227,8 @@
                                 <div class="form-group">
                                     <div class="col-10">
                                     <label>Card Type</label> &nbsp;&nbsp; <i class="fa fa-cc-mastercard" aria-hidden="true"></i> &nbsp; 
-
+=======
+>>>>>>> 4de87fb985be4840f699eecee4bfddcbbc0132d7
                                     <i class="fa fa-cc-visa" aria-hidden="true"></i>&nbsp;
                                     <i class="fa fa-cc-amex" aria-hidden="true"></i>&nbsp;
                                     <i class="fa fa-google-wallet" aria-hidden="true"></i>
@@ -174,35 +238,40 @@
 
                                 </div>
                                 <div class="form-group">
-
-                                    
-
+<<<<<<< HEAD
+                                    <div class="col-10">
+=======
                                     <div class="col-8">
 
-
+>>>>>>> 4de87fb985be4840f699eecee4bfddcbbc0132d7
                                     <label>Card number</label>
                                     <input class="form-control" placeholder="Enter email">
                                     </div>
                                 </div>
                                 <div class="form-group">
-
-                                    
+<<<<<<< HEAD
+                                    <div class="col-10">
+=======
                                     <div class="col-8">
 
-
+>>>>>>> 4de87fb985be4840f699eecee4bfddcbbc0132d7
                                     <label>CVV</label>
                                     <input class="form-control">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    
+<<<<<<< HEAD
+                                    <div class="col-10">
+=======
                                     <div class="col-8">
-
+>>>>>>> 4de87fb985be4840f699eecee4bfddcbbc0132d7
                                     <label>Country zip code</label>
                                     <input class="form-control" placeholder="zip code">
                                     </div>
                                 </div>
-
+<<<<<<< HEAD
+                                
+=======
                                 <div class="col-md-12 text-center">
                                 <button type="submit" class="btn btn-primary">Confirm</button>
                                                 </div>
@@ -218,7 +287,10 @@
 
 
 
-
+>>>>>>> 4de87fb985be4840f699eecee4bfddcbbc0132d7
+                            </form>      </div>
+                                        </div>
+                                    </div>
                                     <div class="card">
                                         <div class="card-header" id="headingTwo">
                                         <h2 class="mb-0">
@@ -279,5 +351,16 @@
                                                     </div>
                                                 </div>
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+>>>>>>> 58c874b (bookings page)
+=======
+        </div>
+
+
+
+>>>>>>> 4de87fb985be4840f699eecee4bfddcbbc0132d7
 
 </div>
