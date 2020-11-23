b437991f1d01556f074aa081c485e31ff56db53c
erickngumbau <57805450+erickngumbau@users.noreply.github.com>
2020-11-23 22:34:44 +0300

    merged profile

--- frontend/views/profile/profile.php
+++ frontend/views/profile/profile.php
@@ -1,5 +1,7 @@
+<?php
+use yii\widgets\ActiveForm;
 
-
+?>
   <nav >
   <ol class="breadcrumb background">
     <li class="breadcrumb-item"><a href="#">Home</a></li>
@@ -17,7 +19,9 @@
     <i class="fa fa-user-circle-o" aria-hidden="true" style="font-size:120px;padding: 10px"></i>
   </div>
     <div class="row justify-content-center" style="font-size:0.83em;">
-      <a href="#" class="link" >Update photo</a>
+      <div>
+      <input type="file" id="file">  Update photo
+      </div>
     </div>
     <div class="card-body">
       <i class="fa fa-check-square-o" aria-hidden="true" style="font-size:50px"></i>
@@ -26,6 +30,7 @@
       <p class="card-text font-weight-light">Show others you’re really you with the identity verification badge.</p>
       <button type="button" class="btn btn-outline-secondary">Get The Badge</button>
     </div>
+    <br></br>
     <div class="card-footer">
       <div class="text" style="font-size: 1.2rem">
       <p><strong>Sam Smith Confirmed</strong></p>
@@ -34,7 +39,33 @@
     </div>
   </div>
 </div>
+
+
 </div>
 
 </div>
+</div>
+
+<?= $form->field($model, 'userId')->textInput() ?>
+
+<?= $form->field($model, 'isCompany')->textInput(['maxlength' => true]) ?>
+
+<?= $form->field($model, 'isSupplier')->textInput(['maxlength' => true]) ?>
+
+<?= $form->field($model, 'countryId')->textInput() ?>
+
+<?= $form->field($model, 'profileImagePath')->textInput() ?>
+
+<?= $form->field($model, 'fullNames')->textInput(['maxlength' => true]) ?>
+
+<?= $form->field($model, 'phone')->textInput() ?>
+
+<?= $form->field($model, 'dateOfBirth')->textInput() ?>
+
+<?= $form->field($model, 'address')->textarea(['rows' => 6]) ?>
+
+<?= $form->field($model, 'status')->textInput() ?>
+
+<div class="form-group">
+    <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
 </div>
